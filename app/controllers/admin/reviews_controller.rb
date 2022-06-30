7bdbbced597997d0f15b37bcd49663958c325fc3
u1856 <sglspy856@gmail.com>
2022-06-30 16:49:33 +0900

    作品投稿ページを作成

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -2,7 +2,17 @@ class Admin::ReviewsController < ApplicationController
   before_action :if_not_admin
 
   def new
+    @review = Review.new
+  end
 
+  def create
+    @review = Review.new(review_params)
+    if @review.valid?
+      @review.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
 
@@ -11,6 +21,10 @@ class Admin::ReviewsController < ApplicationController
   
   def if_not_admin
     redirect_to root_path unless current_user.admin?
+  end
+
+  def post_params
+    params.require(:review).permit(:movie, :summary, :category_id).merge(user_id: current_user.id)
   end
 
 end

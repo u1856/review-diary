af5779d4ab06c3b7a9f863518683020a97d28681
u1856 <sglspy856@gmail.com>
2022-06-30 12:45:51 +0900

    管理者用のデータを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  #get 'reviews/index'
+
   root "reviews#index"
 
   resources :reviews
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2022_06_29_105954) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", null: false
+    t.boolean "admin", default: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

bae48f6c14ab8efa2f888e2631eb174f5f18cd0b
u1856 <sglspy856@gmail.com>
2022-07-08 21:40:34 +0900

    アソシエーションを記述

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :movie
   belongs_to :user
+  has_many :favorites, dependent: :destroy
 
   validates :rate, presence: true
-  
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :movies
   has_many :reviews, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   
 end

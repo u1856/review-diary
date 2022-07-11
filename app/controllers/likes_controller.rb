706ce11bd152e686a45929fd43ec6e63cc93ba66
u1856 <sglspy856@gmail.com>
2022-07-11 17:18:31 +0900

    likesコントローラーを作成

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,22 @@
+class LikesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @like = @movie.likes.new(user_id: current_user.id)
+    if @like.save
+    else
+      redirect_to request.referer
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:movie_id])
+    @like = @movie.likes.find_by(user_id: current_user.id)
+    if @like.present?
+      @like.destroy
+    else
+      redirect_to request.referer
+    end
+  end
+end

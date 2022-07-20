2499ed79ac066a29ab777cd2a5e1a4ae9cb5a2cd
u1856 <sglspy856@gmail.com>
2022-07-21 00:50:52 +0900

    不要なコメントを削除

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :movie, optional: true
   belongs_to :user
-  has_many :favorites#, dependent: :destroy
-  #has_many :favorite_users, through: :favorites, source: :user
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
 
   validates :rate, presence: true
 
